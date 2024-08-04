cb2037fee231787bf09ead6afafd971cbb4f4041
月落星河Tsukistar <tsukistar@tsukistar.fun>
2024-08-05 00:04:17 +0800

    增加workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://tsukistar.github.io',
+	base: 'astro-blog-plus',
 	integrations: [mdx(), sitemap()],
 });
